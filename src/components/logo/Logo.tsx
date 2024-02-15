c7f06bb8cb0a8e83e9b7b503daff6bc38ad735ce
Matsvei Khmialnitski <geniusx1990@gmail.com>
2024-02-15 13:21:17 +0300

    fix: updated mobile version

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,48 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import './style.css';
+import React from 'react';
 import logoImage from '/fit.svg';
 import logoName from '/Clever.svg';
-import './style.css';
-import mobileLogo from '/logo_mobile.svg';
-
 interface LogoProps {
     collapsed: boolean;
 }
 
 const Logo: React.FC<LogoProps> = ({ collapsed }) => {
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 361);
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    if (isMobile) {
-        return (
-            <div className='logo'>
-                <img src={mobileLogo} alt='Mobile Logo' />
-            </div>
-        );
-    } else {
-        return (
-            <div
-                className='logo'
-                style={{
-                    paddingLeft: collapsed ? '18px' : '29px',
-                    paddingRight: collapsed ? '17.45px' : '46px',
-                }}
-            >
-                {!collapsed && <img src={logoName} alt='Clever' />}
-                <img src={logoImage} alt='Clever Logo' />
-            </div>
-        );
-    }
+    return (
+        <div
+            className='logo'
+            style={{
+                paddingLeft: collapsed ? '18px' : '29px',
+                paddingRight: collapsed ? '17.45px' : '46px',
+            }}
+        >
+            {!collapsed && <img className='first-img' src={logoName} alt='Clever' />}
+            <img className='second-img' src={logoImage} alt='Clever Logo' />
+        </div>
+    );
 };
 
 export default Logo;
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
 import './main-page.css';
+import React, { useState } from 'react';
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
 import MainSection from '@components/MainSection/MainSection';
 import { Layout } from 'antd';
 import Sider from 'antd/lib/layout/Sider';
+import Logo from '@components/Logo/Logo';
 import { Content } from 'antd/lib/layout/layout';
 import CustomMenu from '@components/CustomMenu/CustomMenu';
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import ButtonExit from '@components/ButtonExit/ButtonExit';
-import Logo from '@components/Logo/Logo';
 
 export const MainPage: React.FC = () => {
     const [collapsed, setCollapsed] = useState(false);
@@ -17,13 +17,23 @@ export const MainPage: React.FC = () => {
     const onCollapse = (collapsed: boolean) => {
         setCollapsed(collapsed);
     };
-    const dataTestId = window.innerWidth <= 361 ? 'sider-switch-mobile' : 'sider-switch';
 
     return (
         <Layout style={{ minHeight: '100vh' }}>
             <div
-                data-test-id={dataTestId}
+                data-test-id='sider-switch'
                 className={`trigger ${collapsed ? 'collapsed' : ''}`}
+                onClick={() => onCollapse(!collapsed)}
+            >
+                {collapsed ? (
+                    <MenuFoldOutlined style={{ color: '#8C8C8C' }} />
+                ) : (
+                    <MenuUnfoldOutlined style={{ color: '#8C8C8C' }} />
+                )}
+            </div>
+            <div
+                data-test-id='sider-switch-mobile'
+                className={`trigger-mobile ${collapsed ? 'collapsed' : ''}`}
                 onClick={() => onCollapse(!collapsed)}
             >
                 {collapsed ? (
